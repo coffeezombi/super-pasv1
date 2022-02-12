aa3eacea7fc8a4e43b64c39b5359759ed7a05ddd
coffeezombi <87940943+coffeezombi@users.noreply.github.com>
2022-02-11 23:38:31 -0500

    add auth helper (#6)

--- config/setup.js
+++ config/setup.js
@@ -1,13 +1,10 @@
 import 'dotenv/config'
-import supertest from 'supertest'
+import AuthHelper from "../helpers/auth.helper";
 
-    before ( async function() {
-        const request = supertest(process.env.BASE_URL)
-        await request
-            .post('/auth')
-            .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
-            .then(res => {
-                process.env['TOKEN'] = res.body.token
-            })
+before(async function () {
+    const authHepler = new AuthHelper()
+    await authHepler.login(process.env.LOGIN, process.env.PASSWORD)
 
+    process.env['TOKEN'] = authHepler.response.body.token
 })
+
