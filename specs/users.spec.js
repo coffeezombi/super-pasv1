ed4f25a8154a38b7a3b955e26b5e328298a8aaf2
coffeezombi <anastasiiafomina88@gmail.com>
2022-02-19 00:11:45 -0500

    add mock server

--- specs/users.spec.js
+++ specs/users.spec.js
@@ -1,7 +1,6 @@
 import UsersHelper from '../helpers/users.helper';
 import { getRandomItem } from '../helpers/common.helper'
 import { expect } from 'chai'
-import ConfigHelper from "../helpers/config.helper";
 
 describe('Users', function () {
     let userHelper = new UsersHelper()
@@ -12,10 +11,7 @@ describe('Users', function () {
         userId = userHelper.response.body.id
     })
 
-    after(async function () {
-        const configHelper = new ConfigHelper()
-        await configHelper.wipeDate()
-    })
+
 
     describe('user creation', function () {
 
